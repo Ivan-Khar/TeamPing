e9ee37151198ee71c2439aabc9992230772280b5
AqUpd <aquupd@ya.ru>
2022-07-25 13:34:10 +0300

    change version

--- src/main/java/com/aqupd/teamping/TeamPing.java
+++ src/main/java/com/aqupd/teamping/TeamPing.java
@@ -22,7 +22,7 @@ public class TeamPing {
 
 	public static final String MOD_ID = "teamping";
 	public static final String MOD_NAME = "TeamPing";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.1.1";
 	public static final Logger LOGGER = LogManager.getLogger("AqUpd's " + MOD_NAME);
 	public static final String[] pingidnames = new String[]{"here", "notice", "question", "no", "yes", "defend", "attack", "mine"};
 	public static List<JsonObject> pings = new ArrayList<>();
